8a4c223778f04818b63cf3c1b8c8b430d34de13c
Paaaark <hifipark0801@gmail.com>
2025-01-26 00:01:54 -0500

    Applications Closed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -19,18 +19,36 @@ const Main = () => {
       <HighlightGroup />
 
       {/* Join Us Section */}
-      <section className="py-r-10 px-r-5 bg-gray-100 text-center">
-        <h2 className="text-r-lg font-bold mb-r-2 font-catchy">Join Us</h2>
-        <p className="text-r-sm-3xl mb-r-3">
-          The application for Spring 2025 is open!
-        </p>
-        <Link
-          to="/join-us"
-          className={`inline-block ${theme.dust} ${theme.text_black} ${theme.hover_dust} text-r-sm font-semibold py-r-1 px-r-2 rounded-lg transition-colors`}
-        >
-          Join Us Now
-        </Link>
-      </section>
+      <div className="bg-gray-100 flex flex-row justify-center">
+        <section className="py-r-10 px-r-5 bg-gray-100 text-center w-full max-w-7xl">
+          <h2 className="text-r-lg font-bold mb-r-2 font-catchy">Join Us</h2>
+          <p className="text-r-sm-3xl mb-r-3 font-semibold">
+            Applications for Spring 2025 Are Now Closed.
+          </p>
+          <h2 className="text-r-sm-3xl mb-1 lg:mb-3 font-semibold">
+            What's Next?
+          </h2>
+          <p className="text-r-xs-2xl mb-2 lg:mb-4">
+            Our team is currently reviewing all applications, and we’ll be
+            reaching out to applicants shortly. <br></br>
+            For a detailed timeline, please visit our "Join Us" page.
+          </p>
+          <Link
+            to="/join-us"
+            className={`inline-block ${theme.dust} ${theme.text_black} ${theme.hover_dust} text-r-sm font-semibold py-r-1 px-r-2 rounded-lg transition-colors mb-r-3`}
+          >
+            Join Us Page
+          </Link>
+          <p className="text-r-sm-3xl lg:mb-3 font-semibold">
+            Missed the Deadline?
+          </p>
+          <p className="text-r-xs-2xl mb-r-3">
+            While applications for this cycle are closed, we’d love for you to
+            consider applying next semester. <br></br> Be sure to check back
+            here or follow us on Instagram and LinkedIn for updates!
+          </p>
+        </section>
+      </div>
     </div>
   );
 };
--- src/pages/JoinUs/JoinUs.js
+++ src/pages/JoinUs/JoinUs.js
@@ -9,14 +9,15 @@ const JoinUs = () => {
       className={`py-r-10 ${theme.background} ${theme.text_white} flex flex-col items-center space-r-y-4`}
     >
       <h3 className="text-r-lg font-semibold font-frank text-center">
-        The application for Spring 2025 is open!
+        Applications for Spring 2025 Are Now Closed.
       </h3>
-      <Link
-        to="https://docs.google.com/forms/d/e/1FAIpQLSeNhEXHysQK5NW-FYsYjXINs-wC3tPNCSJqgQpjE6OmAgwQlQ/viewform"
-        className={`inline-block ${theme.dust} ${theme.text_black} ${theme.hover_dust} text-r-sm font-semibold py-r-1 px-r-2 rounded-lg transition-colors`}
-      >
-        Join Us Now
-      </Link>
+      <h2 className="text-r-sm-3xl mb-1 lg:mb- font-semibold text-center">
+        What's Next?
+      </h2>
+      <p className="text-r-xs-2xl mb-2 lg:mb-3 text-center">
+        Our team is currently reviewing all applications, and we’ll be reaching
+        out to applicants shortly.
+      </p>
       <h2 className="text-r-lg font-bold font-frank text-center">
         Spring 2025 Recruitment Timeline
       </h2>
